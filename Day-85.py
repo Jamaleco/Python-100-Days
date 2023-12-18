8699dd691e2f0786a3920901f89599a3c263d72b
David Emanuel Silveira <115031797+Jamaleco@users.noreply.github.com>
2023-12-18 19:44:09 -0300

    Add files via upload
    Made with PyQt6 lib instead of Tkinter, because I was learning it in college.

---
+++ Day-85.py
@@ -0,0 +1,122 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QFileDialog, QWidget, QLineEdit, QDialog, QHBoxLayout, QMessageBox
+from PyQt6.QtGui import QPixmap, QImage, QPainter, QFont
+from PyQt6.QtCore import Qt, QRect
+
+class WatermarkDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle("Configurar Marca D'Água")
+
+        self.watermark_text_input = QLineEdit(self)
+        self.watermark_text_input.setPlaceholderText("Digite o texto da marca d'água")
+
+        ok_button = QPushButton("OK", self)
+        ok_button.clicked.connect(self.accept)
+
+        cancel_button = QPushButton("Cancelar", self)
+        cancel_button.clicked.connect(self.reject)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.watermark_text_input)
+        layout.addWidget(ok_button)
+        layout.addWidget(cancel_button)
+
+        self.setLayout(layout)
+
+    def get_watermark_text(self):
+        return self.watermark_text_input.text()
+
+class WatermarkApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.image_path = None
+
+        # GUI components
+        self.image_label = QLabel(self)
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        select_button = QPushButton("Selecionar Imagem", self)
+        select_button.clicked.connect(self.select_image)
+
+        watermark_button = QPushButton("Adicionar Marca D'Água", self)
+        watermark_button.clicked.connect(self.add_watermark)
+
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.image_label)
+        layout.addWidget(select_button)
+        layout.addWidget(watermark_button)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+
+        self.setCentralWidget(central_widget)
+
+    def select_image(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setNameFilter("Imagens (*.png *.jpg *.jpeg *.gif *.bmp)")
+        file_path, _ = file_dialog.getOpenFileName()
+
+        if file_path:
+            self.image_path = file_path
+            self.display_image()
+
+    def display_image(self):
+        pixmap = QPixmap(self.image_path)
+        self.image_label.setPixmap(pixmap)
+
+    def add_watermark(self):
+        if self.image_path:
+            # Pergunta ao usuário o texto da marca d'água
+            watermark_dialog = WatermarkDialog(self)
+            result = watermark_dialog.exec()
+
+            if result == QDialog.DialogCode.Accepted:
+                watermark_text = watermark_dialog.get_watermark_text()
+
+                original_image = QImage(self.image_path)
+
+                # Crie uma cópia da imagem para evitar modificar a original durante o processo de pintura
+                modified_image = QImage(original_image)
+
+                # Adiciona a marca d'água na cópia
+                font = QFont()
+                font.setPixelSize(20)  # Tamanho da fonte
+
+                painter = QPainter(modified_image)
+                painter.setPen(Qt.GlobalColor.black)
+                painter.setFont(font)
+
+                # Calcula as coordenadas para a parte inferior direita
+                font_metrics = painter.fontMetrics()
+                text_rect = font_metrics.boundingRect(QRect(0, 0, 0, 0), Qt.AlignmentFlag.AlignLeft, watermark_text)
+                x = modified_image.width() - text_rect.width() - 10  # 10 pixels da borda direita
+                y = modified_image.height() - text_rect.height() - 10  # 10 pixels da borda inferior
+
+                painter.drawText(x, y, watermark_text)
+
+                # Salva a cópia da imagem com a marca d'água
+                output_path, _ = QFileDialog.getSaveFileName(self, "Salvar Imagem", "", "Imagens PNG (*.png)")
+
+                if output_path:
+                    modified_image.save(output_path)
+
+                    self.image_path = output_path
+                    self.display_image()
+
+                    # Mostra uma mensagem de sucesso
+                    QMessageBox.information(self, "Sucesso", "Marca d'água adicionada com sucesso!")
+
+                    # Encerra o programa
+                    sys.exit()
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = WatermarkApp()
+    window.show()
+    sys.exit(app.exec())
